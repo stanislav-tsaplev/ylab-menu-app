9b48c45c5d1a48b70afb220dd58b08ed4485fb19
Stanislav Tsaplev <stanislav.tsaplev@gmail.com>
2023-01-20 20:32:49 +0300

    Move the routes' urls out to separate file

---
+++ app/endpoints.py
@@ -0,0 +1,6 @@
+ROUTE_PREFIXES = {
+    "base": "/api/v1",
+    "menu": "/api/v1/menus",
+    "submenu": "/api/v1/menus/{menu_id}/submenus",
+    "dish": "/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes",
+}
