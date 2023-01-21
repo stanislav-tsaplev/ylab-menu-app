8e6bce16b790f7c79335f58e4d31f5a33c04ef57
Stanislav Tsaplev <stanislav.tsaplev@gmail.com>
2023-01-21 19:53:53 +0300

    Fix menu tests according to spec

--- tests/v1/resources/menu.py
+++ tests/v1/resources/menu.py
@@ -3,16 +3,33 @@ creating_menu_data = {
     "description": "My menu description 1"
 }
 
+created_menu_data = {
+    "id": None,
+    "title": "My menu 1",
+    "description": "My menu description 1",
+    "submenus_count": 0,
+    "dishes_count": 0,
+}
+
+read_menu_data = {
+    "id": None,
+    "title": "My menu 1",
+    "description": "My menu description 1",
+    "submenus_count": 0,
+    "dishes_count": 0,
+}
+
 updating_menu_data = {
     "title": "My updated menu 1",
     "description": "My updated menu description 1",
 }
 
-read_menu_data = {
-    "title": "My menu 1",
-    "description": "My menu description 1",
+updated_menu_data = {
+    "id": None,
+    "title": "My updated menu 1",
+    "description": "My updated menu description 1",
     "submenus_count": 0,
-    "dishes_count": 0
+    "dishes_count": 0,
 }
 
 deleted_menu_response = {
